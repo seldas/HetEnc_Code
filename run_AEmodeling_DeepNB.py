eb419998ba0b8e747d7e11ba4acdf2567fcc168b
seldas <lan.seldas@Gmail.com>
2019-12-09 07:57:12 -0600

    the latest version for main code

--- run_AEmodeling_DeepNB.py
+++ run_AEmodeling_DeepNB.py
@@ -4,16 +4,10 @@ import pandas as pd
 import os
 from sklearn.preprocessing import StandardScaler as Norm
 
-AE_ver = 'v3'
-if AE_ver is 'v1':
-	from Scripts.SrcAutoEncoder_V1 import * # 
-elif AE_ver is 'v2':
-	from Scripts.SrcAutoEncoder_V2 import * #		
-elif AE_ver is 'v3':
-	from Scripts.SrcAutoEncoder_V3 import * # V3 is used currently in the project.		
+from .SrcAutoEncoder_V3 import * 
 
 
-def main(curr_wd = './DeepNB/', data_format = 'Raw'):		
+def main(curr_wd = './Data/', data_format = 'Norm'):		
 	#### Load dataset #####
 	(x_1, x_2) = AE_data_loading(curr_wd, data_format)
 	#######################
@@ -21,16 +15,16 @@ def main(curr_wd = './DeepNB/', data_format = 'Raw'):
 	###################################
 	##### Parameter Setting ###########
 	batch_size = x_1.shape[0]
-	verbose = 0
+	verbose = 2
 	randSeed= 2018
 	epochs  = 1000
-	featureOut = 1024
+	featureOuts = [1024]
 	acti_funcs = ['tanh']
 	drop_rates = [2]
 	###################################
 	###################################
 	
-	param_cache = (curr_wd, batch_size, verbose, randSeed, epochs, featureOut, acti_funcs, drop_rates, data_format)
+	param_cache = (curr_wd, batch_size, verbose, randSeed, epochs, featureOuts, acti_funcs, drop_rates, data_format)
 	AE_modeling(x_1, x_2, param_cache)
 
 
@@ -52,24 +46,27 @@ def AE_data_loading(curr_wd, data_format):
 	return (x_1, x_2)
 
 def AE_modeling(x_1, x_2, param_cache):
-	(curr_wd, batch_size, verbose, randSeed, epochs, featureOut, acti_funcs, drop_rates, data_format) = param_cache
-	for drop_rate_1 in drop_rates:
-		for acti_func in acti_funcs:
-			drop_rate = drop_rate_1/10
-			curr_Dir = (curr_wd + "Models/"+ data_format + '_' + acti_func+'_'+
-						 'DROPOUT'+str(drop_rate_1)+'_'+
-						 str(epochs)+'EPO_'+ str(featureOut)+'FEATS_'+ AE_ver+"_" +str(randSeed)+ "/")
+	(curr_wd, batch_size, verbose, randSeed, epochs, featureOuts, acti_funcs, drop_rates, data_format) = param_cache
+	for featureOut in featureOuts:
+		for drop_rate_1 in drop_rates:
+			for acti_func in acti_funcs:
+				print('features: ', featureOut,' drop-out: ', drop_rate_1,' activate function: ', acti_func)
+				drop_rate = drop_rate_1/10
+				curr_Dir = (curr_wd + "Models/"+ data_format + '_' + acti_func+'_'+
+							 'DROPOUT'+str(drop_rate_1)+'_'+
+							 str(epochs)+'EPO_'+ str(featureOut)+'FEATS_'+ AE_ver+"_" +str(randSeed)+ "/")
 
-			try:
-				os.stat(curr_Dir)
-			except:
-				os.mkdir(curr_Dir)
-				print('Start Running ', curr_Dir, '.')
+				try:
+					os.stat(curr_Dir)
+				except:
+					os.mkdir(curr_Dir)
+					print('Start Running ', curr_Dir, '.')
 
-			AE_models = train_AEModels(x_1, x_2, curr_Dir, drop_rate = drop_rate,
-				 randSeed = randSeed, epochs=epochs, batch_size = batch_size, featureOut = featureOut, acti_func=acti_func,
-				 verbose = verbose)
+				AE_models = train_AEModels(x_1, x_2, curr_Dir, drop_rate = drop_rate,
+					 randSeed = randSeed, epochs=epochs, batch_size = batch_size, featureOut = featureOut, acti_func=acti_func,
+					 verbose = verbose)
 
 ######################################
 
-main()
+if __name__ == '__main__':
+	main()
